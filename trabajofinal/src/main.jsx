6349de8bf889b597996e3fa4380985ebe28a1cb7
Renzo <Barrazarenzo645@gmail.com>
2024-09-12 20:24:24 -0300

    Idea de la idea

--- trabajofinal/src/main.jsx
+++ trabajofinal/src/main.jsx
@@ -4,19 +4,12 @@ import { createRoot } from 'react-dom/client'
 import { Botoninicio } from './boton inicio/boton.jsx'
 import { Linea } from './nav/linea.jsx'
 import './nav/nav.css'
-
+import { Nav2 } from './nav/nav.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
- <nav >
-            <ul>
-            <li><a href="">Messi       </a> </li>
-            <li><a href="">JULIETANEGRA</a>  </li>
-            <li><a href="">SOY MESSI   </a> </li>
-            </ul>
-            </nav>
+<Nav2/>
             <Linea/>
             <Botoninicio/>
           
-  </StrictMode>,
+  </StrictMode>
 )
---
+++ trabajofinal/src/nav/nav.jsx
@@ -0,0 +1,14 @@
+import './nav.css'
+export function Nav2 ()
+{
+return(   <nav className='navR Re' >
+    <ul className='ulR'>
+    <li className='liR'><a href="" className='aR'>Messi       </a> </li>
+    <li className='liR'><a href="" className='aR'>JULIETANEGRA</a>  </li>
+    <li className='liR'><a href="" className='aR'>SOY MESSI   </a> </li>
+    </ul> '
+    </nav>)
+ 
+}
+          
+          
